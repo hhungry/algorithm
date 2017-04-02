acb43951c8dd2fcf3399c4ae2c55334949805fe7
pyq <1014280613@qq.com>
2017-04-02 19:54:28 +0800

    all combination of char array

---
+++ algorithm/combination.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<vector>
+#include<sstream>
+#include<string>
+#include<cstring>
+using namespace std;
+
+
+//all combination
+void printAllCombination(char *str){
+    if(!str||*str=='\0')
+        return ;
+    int len=strlen(str);
+    int n=1<<len;
+    for(int i=1;i<n;++i){
+        for(int j=0;j<len;++j){
+            int tmp=i;
+            if(tmp&(1<<j))
+                cout<<str[j];
+        }
+        cout<<endl;
+    }
+}
+int main(){
+   char str[]="abcd";
+   printAllCombination(str);
+}
+
