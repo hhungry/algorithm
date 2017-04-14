a91e11e6b21c0505399358c1822942f82d21f464
pyq <1014280613@qq.com>
2017-04-14 21:16:08 +0800

    delete all node

---
+++ algorithm/deleteAllSameNode.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+typedef struct listNode{
+    int val;
+    struct listNode *next;
+}listNode,*LinkList;
+
+void deleteAllSameNode(ListNode **head,int data){
+    if(head==NULL||*head==NULL)
+        return ;
+    ListNode *pre=NULL;
+    ListNode *curr=*head;
+    ListNode *pnext=NULL;
+    while(curr&&curr->val==data){
+        pnext=curr->next;
+        delete curr;
+    }
+    if(curr==NULL){
+        *head=NULL;
+        return ;
+    }
+    else{
+        *head=curr;
+        pre=curr;
+        curr=curr->next;
+    }
+    while(curr){
+        if(curr->val==data){
+            pnext=curr->next;
+            pre->next=pnext;
+            delete curr;
+            curr=pnext;
+        }
+        else{
+            pre=curr;
+            curr=curr->next;
+        }
+    }
+}
