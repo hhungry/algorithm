bf7e7b4a8754bbda7b437cc2dc9ddd282b5af755
pyq <1014280613@qq.com>
2017-04-25 16:10:49 +0800

    format cout

---
+++ c++/formatCout.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include<cassert>
+#include<cmath>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int m=0,n=0;
+    while(cin>>n>>m){
+        assert(n>=0&&m<=999);
+        assert(m>=0&&m<=999);
+        double result=0.0,pre=(double)n,next=sqrt(pre);
+        for(int i=1;i<=m;++i){
+            result+=pre;
+            pre=next;
+            next=sqrt(pre);
+        }
+        cout<<setiosflags(ios::fixed)<<setprecision(2)<<result<<endl;
+    }
+    return 0;
+}
