3ccd12b4de15ddfecc201f9116ad60362358505b
hhungry <1014280613@qq.com>
2017-08-20 11:38:25 +0800

    Update heapSort.cpp

--- data_structure/heapSort.cpp
+++ data_structure/heapSort.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include<vector>
 using namespace std;
-void heapAdjust(vector<int>&inputArr,int topIndex,int len){
+void heapAdjust(vector<int>&inputArr,int topIndex,int len)
+{
     int topValue=inputArr[topIndex];
     int leftChild=2*topIndex+1;
-    while(leftChild<len){
+    while(leftChild<len)
+    {
         if(leftChild+1<len&&inputArr[leftChild+1]>inputArr[leftChild])
             ++leftChild;
         if(topValue>inputArr[leftChild])
@@ -15,14 +17,22 @@ void heapAdjust(vector<int>&inputArr,int topIndex,int len){
     }
     inputArr[topIndex]=topValue;
 }
-void heapSort(vector<int> &inputArr){
+void heapCreate(vector<int> &inputArr)
+{
     int len=inputArr.size();
     //建堆
-    for(int i=(len-2)/2;i>=0;--i){
+    for(int i=(len-2)/2; i>=0; --i)
+    {
         heapAdjust(inputArr,i,len);
     }
 
-    for(int i=len-1;i>0;i--){
+}
+void heapSort(vector<int> &inputArr)
+{
+    heapCreate(inputArr);
+    int len=inputArr.size();
+    for(int i=len-1; i>0; i--)
+    {
         int tmp=inputArr[0];
         inputArr[0]=inputArr[i];
         inputArr[i]=tmp;
@@ -30,13 +40,33 @@ void heapSort(vector<int> &inputArr){
     }
 
 }
+void push(vector<int> &inputArr,int value)
+{
+    inputArr.push_back(value);
+    int holeIndex=inputArr.size()-1;
+    int parent=(holeIndex-1)/2;
+    while(holeIndex>0&&inputArr[parent]<value)
+    {
+        inputArr[holeIndex]=inputArr[parent];
+        holeIndex=parent;
+        parent=(holeIndex-1)/2;
+    }
+    inputArr[holeIndex]=value;
+}
 int main()
 {
-    vector<int> inputArr{49,38,65,97,76,13,27};
-    heapSort(inputArr);
+    vector<int> inputArr= {49,38,65,97,76,13,27};
+    heapCreate(inputArr);
+   // heapSort(inputArr);
     for(auto ele:inputArr)
         cout<<ele<<" ";
     cout<<endl;
+
+    push(inputArr,100);
+    for(auto ele:inputArr)
+        cout<<ele<<" ";
+    cout<<endl;
+
     cout << "Hello world!" << endl;
     return 0;
 }
