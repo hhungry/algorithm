a931195d4c00f01567202b66177162cc2fdf3421
hhungry <1014280613@qq.com>
2017-08-20 16:22:55 +0800

    Update copyControl.cpp

--- c++/copyControl.cpp
+++ c++/copyControl.cpp
@@ -2,7 +2,7 @@
 //使用引用计数定义行为像指针的类，类似于shared_ptr
 class HasPtr {
 public:
-    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0), use(new size_t(0)) { }
+    HasPtr(const std::string &s = std::string()) : ps(new std::string(s)), i(0), use(new size_t(1)) { }
     HasPtr(const HasPtr &hp) : ps(hp.ps), i(hp.i), use(hp.use) { ++*use; }
     HasPtr& operator=(const HasPtr &rhs) {
         ++*rhs.use;
@@ -24,5 +24,5 @@ public:
 private:
     std::string *ps;
     int i;
-    size_t *use;
+    size_t *use;//记录有多少个对象共享*ps的成员
 };
