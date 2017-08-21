936abcd68203db62a06bf0db74215ab2eb28b627
hhungry <1014280613@qq.com>
2017-08-21 15:04:20 +0800

    获取链表倒数第k个节点

---
+++ data_structure/findKthToTail.cpp
@@ -0,0 +1,26 @@
+//链表中倒数第k个节点
+ListNode* findKthToTail(ListNode *L,int i){
+    if(L==nullptr||L->next==nullptr){
+        invalidInput=true;
+        return nullptr;
+    }
+    ListNode *p=L->next;
+    int j=1;
+    while(p&&j<i){
+        p=p->next;
+        ++j;
+    }
+    if(!p||j>i){
+        invalidInput=true;
+        return nullptr;
+    }
+
+    ListNode *q=L->next;
+    while(p->next!=nullptr){
+        q=q->next;
+        p=p->next;
+    }
+    return q;
+
+}
+
