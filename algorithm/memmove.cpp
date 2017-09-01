60f41866b158118ad4f2bf9b598adf84fcc2fc89
hhungry <1014280613@qq.com>
2017-09-01 12:47:40 +0800

    保证字符串数组最后一个字符为'\0'

--- algorithm/memmove.cpp
+++ algorithm/memmove.cpp
@@ -16,15 +16,17 @@ void* memmove_self(void *des,void *src,size_t len){
     void *ret=des;
     if(des==src)
         return ret;
-    else if(des<src||des>=(char *)src+len){
+    else if(des<src||des>(char *)src+len){
         while(len--){
             *(char *)des=*(char *)src;
             des=(char *)des+1;
             src=(char *)src+1;
         }
+        *(char *)des='\0';
     }else{
-        des=(char *)des+len-1;
-        src=(char *)src+len-1;
+        des=(char *)des+len;
+        src=(char *)src+len;
+        ++len;
         while(len--){
             *(char *)des=*(char *)src;
             des=(char *)des-1;
