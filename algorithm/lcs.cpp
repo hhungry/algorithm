28ee18d87174b47dfcd4a6bd1a02c392b4162a19
hhungry <1014280613@qq.com>
2017-09-10 12:36:41 +0800

    最长公共子串、自序列

--- algorithm/lcs.cpp
+++ algorithm/lcs.cpp
@@ -32,3 +32,27 @@ int main(){
         cout<<deleteNum<<endl;
     }
 }
+
+
+//最长公共子串
+int LCSubstr(char *str1,int len1,char *str2,int len2){
+    int **dp=new int*[len1];
+    for(int i=0;i<len1;++i)
+        dp[i]=new int[len2];
+
+    int maxlen=0;
+    for(int i=0;i<len1;++i){
+        for(int j=0;j<len2;++j){
+            if(str1[i]==str2[j]){
+                if(i&&j)
+                    dp[i][j]=dp[i-1][j-1]+1;
+                else dp[i][j]=1;
+                if(dp[i][j]>maxlen)
+                    maxlen=dp[i][j];
+            }
+            else dp[i][j]=0;
+        }
+    }
+    return maxlen;
+
+}
