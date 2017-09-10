c27d5d86eacc0e3c483b3de2fcc25002c0124b03
hhungry <1014280613@qq.com>
2017-09-10 12:41:32 +0800

    回文子串起始位置

--- algorithm/getLongestPalindrome.cpp
+++ algorithm/getLongestPalindrome.cpp
@@ -70,4 +70,43 @@ public:
         }
         return maxlen;
     }
+    
+    //记录最长子串的起始位置
+    //longest palindrome len and substr
+string longestPalindrome(string s)
+{
+    int i=0,j=0,maxLen=0,c=0;
+    int pbegin=0,pend=0;
+
+    for(i=0;i<s.size();++i)
+    {
+        for(j=0; (i-j)>=0&&(i+j)<s.size(); ++j)
+        {
+            if(s[i-j]!=s[i+j])
+                break;
+            c=2*j+1;
+        }
+        if(c>maxLen)
+        {
+            maxLen=c;
+            pbegin=(i-j+1)>=0?(i-j+1):pbegin;
+            pend=(i+j-1)<s.size()?(i+j-1):pend;
+        }
+
+        for(j=0; (i-j)>=0&&(i+j+1)<s.size(); ++j)
+        {
+            if(s[i-j]!=s[i+j+1])
+                break;
+            c=2*j+2;
+        }
+        if(c>maxLen)
+        {
+            maxLen=c;
+            pbegin=(i-j+1)>=0?(i-j+1):pbegin;
+            pend=(i+j)<s.size()?(i+j):pend;
+        }
+    }
+    string sub=s.substr(pbegin,pend-pbegin+1);
+    return sub;
+}
 };
