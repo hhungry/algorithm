d6d5ef8cc789a666125bbf3c4106555f522c15b0
hhungry <1014280613@qq.com>
2017-09-19 21:45:31 +0800

    Update printMinNumber.cpp

--- algorithm/printMinNumber.cpp
+++ algorithm/printMinNumber.cpp
@@ -22,6 +22,13 @@ bool compare(const string &str1,const string &str2){
     if(j<str2.size())
         return false;
 }
+bool compare(const string &str1,const string &str2){
+    string str1str2=str1+str2;
+    string str2str1=str2+str2;
+    if(str1str2>str2str1)
+        return true;
+    else return false;
+}
 
 void printMinNumber(vector<int> &vec){
     if(vec.size()==0)
